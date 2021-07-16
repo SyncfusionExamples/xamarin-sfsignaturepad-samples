67f36eeb736584f2e1c7c45e28fca9c58fc266f2
Paul Anderson <42271912+PaulAnderson02@users.noreply.github.com>
2021-07-16 10:23:41 +0530

    ImageSource usage sample

---
+++ ImageSource/SignaturePadSample/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SignaturePadSample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            signature.Save();
+            StreamImageSource streamImageSource = (StreamImageSource)signature.ImageSource;
+            System.Threading.CancellationToken cancellationToken =
+            System.Threading.CancellationToken.None;
+            Task<Stream> task = streamImageSource.Stream(cancellationToken);
+            Stream stream = task.Result;
+
+            byte[] bytes = new byte[stream.Length];
+            stream.Read(bytes, 0, bytes.Length);
+
+            // Store bytes to server
+        }
+    }
+}
